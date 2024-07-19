de25c4ac1d6258a9cc78520495ed51dea10e273f
Benjamin C <2361255+bchartier@users.noreply.github.com>
2024-07-19 12:33:09 +0200

    style: add noqa tag in __init__.py file

--- plugin/idg/toolbelt/__init__.py
+++ plugin/idg/toolbelt/__init__.py
@@ -2,8 +2,8 @@
 from .log_handler import PlgLogger  # noqa: F401
 from .preferences import PlgOptionsManager  # noqa: F401
 from .translator import PlgTranslator  # noqa: F401
-from .plugin_globals import PluginGlobals
-from .singleton import Singleton
-from .browser import IdgProvider
-from .remote_platforms import RemotePlatforms
+from .plugin_globals import PluginGlobals  # noqa: F401
+from .singleton import Singleton  # noqa: F401
+from .browser import IdgProvider  # noqa: F401
+from .remote_platforms import RemotePlatforms  # noqa: F401
 from .network_manager import NetworkRequestsManager  # noqa: F401
